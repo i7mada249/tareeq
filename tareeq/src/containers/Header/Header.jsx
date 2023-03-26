2fdd35dd7c25e36dd461878482d659dc7e6b2748
i7mada249 <i7mada249@outlook.com>
2023-03-25 17:01:04 -0700

    2

---
+++ tareeq/src/containers/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header section__padding'>
+
+        <div className='header-content'>
+            <h1 className='gradient__txt'>خارطة طريقك لتصبح مطورا</h1>
+            <p>شاهد خارطة الطريق لجميع التخصصات ولغات البرمجة بشكل مبسط وسهل لتجعل رحلة تعلمك أسهل</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
